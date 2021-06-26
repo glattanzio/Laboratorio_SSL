336d179417eab6693739ec1ad75718d40039d458
Tomas Rodriguez <tomirodriguez1368@gmail.com>
2021-06-26 12:59:12 -0300

    Inicio

---
+++ .idea/main.py
@@ -0,0 +1,24 @@
+cadena = "aux1 = 12 para i desde 9 hasta 19 {mostrar aux1 + i}"
+def analizar_palabra(n):
+    t_id = es_id(n)
+    t_cte = es_cte(n)
+    t_operador = es_operador(n)
+    t_parenizq = n=="("
+    t_parender = n==")"
+    t_corizq = n =="{"
+    t_corder = n== "}"
+
+def es_id(n):
+def es_cte(n):
+def es_operador(n):
+def es_parentizq(n):
+def es_parentder(n):
+def es_corizq(n):
+def es_corder(n):
+
+for i in range(len(cadena)):
+    if cadena[i]==" ":
+        n =cadena[0:i]
+        analizar_palabra(n)
+    analizar palabra
+    reemplazar token'''
